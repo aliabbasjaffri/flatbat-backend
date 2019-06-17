06ad18544477432618762ecc9f88f515dd4c37e5
Ali Abbas Jaffri <ga53yen@mytum.de>
2019-06-17 11:50:25 +0200

    PORT configuration added
    User can now set a PORT in .env file and the application will be served on that port

--- src/utils/secrets.ts
+++ src/utils/secrets.ts
@@ -9,11 +9,13 @@ if (fs.existsSync(".env")) {
 	});
 }
 
+export const PORT = process.env["PORT"];
 export const SESSION = process.env["SESSION_SECRET"];
 export const MONGO_URI = process.env["MONGODB_URI_LOCAL"];
 export const JWT_SECRET = process.env["JWT_SESSION_SECRET"];
 
 
-if ( !MONGO_URI || !SESSION || !JWT_SECRET) {
+if ( !MONGO_URI || !SESSION || !JWT_SECRET || !PORT ) {
+	console.log(".env file has missing attributes");
 	process.exit(1);
 }
